c482a6820e2a1178ee192fc18ad6f462827ec7d3
Ed <ed@hothouse.co.nz>
2025-05-08 09:14:51 +1200

    remove old project

--- config/tooltip.ts
+++
@@ -1,7 +0,0 @@
-export const tooltipConfig = {
-  // Verzögerung in Millisekunden, bevor ein Tooltip angezeigt wird
-  delayDuration: 400,
-
-  // Verzögerung in Millisekunden, wenn der Benutzer zwischen Tooltip-Triggern wechselt
-  skipDelayDuration: 200,
-}
